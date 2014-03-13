eb4f062659060c078093357247c50ed2480bf9ce
Daniel Galvez <dt.galvez@gmail.com>
2014-03-13 15:30:49 -0400

    Removed cruft. Getting 'objects are not aligned' error when trying to use score(X,y)

--- Python/ReadData.py
+++ Python/ReadData.py
@@ -12,20 +12,9 @@ DATA_DIR = os.getcwd() + '/../data/'
 
 def main():
     clf = LogisticRegression()
-    
     train(DATA_DIR + '/train_1000_v.csv',clf)
     test(DATA_DIR +'/test_1000_v.csv',clf)
-    '''
-    i_f = open( '../data/train_1000.csv', 'r' ) #Make file smaller.
-    reader = csv.reader(i_f)
-    next(i_f) #Skip header
-    #Load files into hash table.
-    call_dict = {} #fileName.wav : isWhaleCall
-    for line in reader:
-        label = line[0]
-        isWhale = line[1]
-        call_dict[label] = isWhale;
-    '''
+
 def train(trainFile, clf):
     i_f = open(trainFile)
     reader = csv.reader(i_f)
@@ -42,11 +31,24 @@ def train(trainFile, clf):
     soundArr = np.array(soundList)
     isWhaleArr = np.array(isWhaleList)#, dtype = type(np.int16))
     clf.fit(soundArr, isWhaleArr)
-    pdb.set_trace()
+    #pdb.set_trace()
 
 def test(testFile, clf):
     i_f = open(testFile)
-    #clf.score(X,y)
+    reader = csv.reader(i_f)
+    soundList = list()
+    isWhaleList = list()
+    for line in reader:
+        fileName = line[0]
+        fileName = fileName[:line[0].find('.aiff')] + '.wav' #Change .aiff ending to .wav
+        (rate, sig) = wav.read(DATA_DIR + 'train/' + fileName)
+        mfcc_feat = mfcc(sig,rate)
+        soundList.append(mfcc_feat)
+        isWhaleList.append(line[1])        
+    soundArr = np.array(soundList)
+    isWhaleArr = np.array(isWhaleList)#, dtype = type(np.int16))
+    
+    pdb.set_trace()
     
 if __name__ == "__main__":
     main()
