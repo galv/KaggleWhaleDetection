242742022831bf7fe244c940842eb658d1e631f0
Daniel Galvez <dt.galvez@gmail.com>
2014-04-27 01:15:50 -0400

    MFCCLearner.py works~

--- Python/MFCCLearner.py
+++ Python/MFCCLearner.py
@@ -10,18 +10,24 @@ import cv2
 
 DATA_DIR = os.getcwd() + '/../data/'
 
+"""
+Training a logistic regression classifier and validates it.
+"""
 def main():
     clf = LogisticRegression()
-    (X_train, y_train) = readTrainData(DATA_DIR + '/train_1000_v.csv',clf)
-    
+    (X_train, y_train, X_val, y_val) = readData(DATA_DIR + '/train.csv',clf)
     clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_train)
-    return (clf, X_train, y_train, y_pred)
+
+    y_val_pred = clf.predict(X_val)
+    
+    successes = np.isclose(y_val, y_val_pred)
+
+    return (clf, X_train, y_train, X_val, y_val, y_val_pred)
 
 """ 
 Returns training data as a tuple (X,y) where X is  an n-by-m unraveled matrix where n is the number of samples, and y is 1-by-n.
 """
-def readTrainData(trainFile, clf):
+def readData(trainFile, clf):
     i_f = open(trainFile)
     reader = csv.reader(i_f)
     soundList = list()
@@ -41,30 +47,24 @@ def readTrainData(trainFile, clf):
     isWhaleArr = np.array(isWhaleList)
     isWhaleArr = isWhaleArr.astype(np.bool)
     #Convert from string to boolean for convenience.
-    return (soundArr, isWhaleArr)
 
-def test(testFile, clf):
-    i_f = open(testFile)
-    reader = csv.reader(i_f)
-    soundList = list()
-    isWhaleList = list()
-    next(i_f)
-    for line in reader:
-        fileName = line[0]
-        fileName = fileName[:line[0].find('.aiff')] + '.wav' #Change .aiff ending to .wav
-        (rate, sig) = wav.read(DATA_DIR + 'train/' + fileName)
-        mfcc_feat = mfcc(sig,rate)
-        soundList.append(mfcc_feat)
-        isWhaleList.append(line[1])
-    soundArr = np.array(soundList)
-    isWhaleArr = np.array(isWhaleList)#, dtype = type(np.int16))
     
-    #predictedWhaleArr = clf.predict(soundArr)
+    #randomize
+    shuffleInUnison(soundArr, isWhaleArr)
     
-    #accuracy = (predictedWhaleArr == isWhaleArr).all()
-    #print accuracy
+    #Split train and validation sets 50-50.
+    trainSoundArr = soundArr[0:len(soundArr)/2]
+    valSoundArr = soundArr[len(soundArr)/2: len(soundArr)]
 
-    return 
+    trainIsWhaleArr = isWhaleArr[0: len(isWhaleArr)/2]
+    valIsWhaleArr = isWhaleArr[len(isWhaleArr)/2: len(isWhaleArr)]
+
+    return (trainSoundArr, trainIsWhaleArr, valSoundArr, valIsWhaleArr)
+
+def shuffleInUnison(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return (a[p], b[p])
     
 if __name__ == "__main__":
-    (clf, X_train, y_train, y_pred) = main()
+    (clf, X_train, y_train, X_val, y_val, y_val_pred) = main()
