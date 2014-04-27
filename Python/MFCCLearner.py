3e70d408ffe5d71afeef9ac8b94a8fd3479056fe
Daniel Galvez <dt.galvez@gmail.com>
2014-04-27 00:23:05 -0400

    Fixing up stuff

--- Python/MFCCLearner.py
+++ Python/MFCCLearner.py
@@ -6,6 +6,7 @@ import scipy.io.wavfile as wav
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from python_speech_features.features import mfcc
+import cv2
 
 DATA_DIR = os.getcwd() + '/../data/'
 
@@ -14,11 +15,11 @@ def main():
     (X_train, y_train) = readTrainData(DATA_DIR + '/train_1000_v.csv',clf)
     
     clf.fit(X_train, y_train)
-    #print clf.predict(X_train)
-    return (clf, X_train, y_train)
+    y_pred = clf.predict(X_train)
+    return (clf, X_train, y_train, y_pred)
 
 """ 
-
+Returns training data as a tuple (X,y) where X is  an n-by-m unraveled matrix where n is the number of samples, and y is 1-by-n.
 """
 def readTrainData(trainFile, clf):
     i_f = open(trainFile)
@@ -28,15 +29,18 @@ def readTrainData(trainFile, clf):
     next(i_f)
     for line in reader:
         fileName = line[0]
-        fileName = fileName[:line[0].find('.aiff')] + '.wav' #Change .aiff ending to .wav
+        fileName = fileName[:line[0].find('.aiff')] + '.wav' 
+        #Change .aiff ending to .wav
         (rate, sig) = wav.read(DATA_DIR + 'train/' + fileName)
         mfcc_feat = mfcc(sig,rate)
-        #pdb.set_trace()
-        mean_mfcc = np.mean(mfcc_feat,1)
-        soundList.append(mean_mfcc)
+        mfcc_feat = np.ndarray.flatten(mfcc_feat)
+        #Unravel the matrix into a 1-D array. 
+        soundList.append(mfcc_feat)
         isWhaleList.append(line[1])
     soundArr = np.array(soundList)
     isWhaleArr = np.array(isWhaleList)
+    isWhaleArr = isWhaleArr.astype(np.bool)
+    #Convert from string to boolean for convenience.
     return (soundArr, isWhaleArr)
 
 def test(testFile, clf):
@@ -63,4 +67,4 @@ def test(testFile, clf):
     return 
     
 if __name__ == "__main__":
-    (clf, X, y) = main()
+    (clf, X_train, y_train, y_pred) = main()
